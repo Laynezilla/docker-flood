2cd6ba277a44a3429827846b7c000dbbb45f3065
Laynezilla <Laynezilla@users.noreply.github.com>
2019-04-20 14:40:47 -0400

    Update docker-entrypoint.sh

--- root/usr/local/bin/docker-entrypoint.sh
+++ root/usr/local/bin/docker-entrypoint.sh
@@ -15,11 +15,4 @@ fi
 
 chown -R $PUSER:$PGROUP "$FLOOD_DIR"
 
-if [ ! -e /v-card ]; then
-	if [ -e /scripts/first-run.sh ]; then
-		. /scripts/first-run.sh
-	fi
-	touch /v-card
-fi
-
 exec su-exec $PUSER:$PGROUP "$@"
