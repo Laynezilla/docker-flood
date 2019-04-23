f11150d6888be19968abb718e1347d65232e74fb
Laynezilla <Laynezilla@users.noreply.github.com>
2019-04-22 23:30:27 -0400

    Update config.js

--- root/flood/config.js
+++ root/flood/config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
 	baseURI: '/',
 	dbCleanInterval: 1000 * 60 * 60,
-	dbPath: '/data/flood-db/',
+	dbPath: '/config/flood-db/',
 	floodServerPort: 3000,
 	maxHistoryStates: 30,
 	pollInterval: 1000 * 5,
 	secret: process.env.FLOOD_SECRET || 'wmtsflumfyegcclx',
 	scgi: {
 		socket: true,
-		socketPath: '/data/rtorrent.sock'
+		socketPath: '/config/rtorrent.sock'
 	}
 };
 
